cc6603c21cedea1a9281cce7932c836170f4091e
Handenur-Kilicarslan <handenur.kilicarslan@bil.omu.edu.tr>
2021-07-31 02:11:44 +0300

    Painting

---
+++ Painting project/Assets/Deneme.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deneme : MonoBehaviour
+{
+
+    public Material matRed;
+    public Material matGreen;
+    public Material matBlue;
+
+    
+
+    // Bu buton için olan kod bunu silme
+    public void RedColor()
+    {
+        GetComponent<Renderer>().material = matRed;
+    }
+
+    public void GreenColor()
+    {
+        GetComponent<Renderer>().material = matGreen;
+    }
+
+    public void BlueColor()
+    {
+        GetComponent<Renderer>().material = matBlue;
+    }
+}
---
+++ Painting project/Assets/Paintable.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.IO;
+using UnityEngine;
+
+public class Paintable : MonoBehaviour
+{
+    
+
+    public GameObject Brush;
+
+    public float BrushSize = 0.1f;
+    
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+           
+
+            //cast a ray to the plane
+            var Ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(Ray, out hit))
+            {
+                
+                //instanciate a brush
+                var go = Instantiate(Brush, hit.point + Vector3.up * 0.1f, Quaternion.identity, transform);
+                go.transform.localScale = Vector3.one * BrushSize;
+                
+            }
+
+        }
+    }
+    
+}
